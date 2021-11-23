421775ffea7e82d69a9f8e00f9561c4fb9f096a7
amonajjemi <monajjemiaaron@gmail.com>
2021-11-22 16:54:04 -0800

    Add files via upload
    edited to sort by Name if salary is equal

--- our_se_v1.cpp
+++ our_se_v1.cpp
@@ -1,3 +1,4 @@
+#include "mitra/Client.h"
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -141,6 +142,7 @@ void cleanReadData(vector<KVpair*> &data)
 	}
 }
 
+//Prints the the name and salary of each KVpair in vector data
 void printKVpairVector(vector<KVpair*> &data)
 {
 	for(int k = 0; (unsigned) k < data.size(); ++k)
@@ -149,18 +151,37 @@ void printKVpairVector(vector<KVpair*> &data)
 	}
 }
 
+void mitraTestCode()
+{
+	bool usehdd = false, cleaningMode = false;
+
+    Server server(usehdd, cleaningMode);
+    Client client(&server, cleaningMode, 100);
+
+    client.update(OP::INS, "test", 5, false);
+    client.update(OP::INS, "test", 6, false);
+    client.update(OP::INS, "test", 7, false);
+    client.update(OP::DEL, "test", 6, false);
+    vector<int> res = client.search("test");
+    
+    for (auto item : res) {
+        cout << item << endl;
+    }
+
+}
+
 int main () {
     
 	vector <KVpair*> data;
 	
 	
 	readFile(data);
+	mergeSort(data, 0 , data.size() - 1);
 	printKVpairVector(data);
 	
-	mergeSort(data, 0 , data.size() - 1);
 	cout << endl << endl;
 	
-	printKVpairVector(data);
+	mitraTestCode();
 	
 	cleanReadData(data);
 	
